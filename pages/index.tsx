2d2029fb4b16ce8d192b85692f1be394891448ee
berkayyilmaz <berkayyilmaz@gmail.com>
2021-08-07 14:06:58 +0300

    Routing bug fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,21 @@
 import { NextPage } from 'next';
-import { RelayEnvironmentProvider } from 'relay-hooks';
-import environment from '../src/relay/environment';
 import React from 'react';
 import Kiosk from './kiosk';
+import { loadSession } from '../src/modules/auth/utils/session.utils';
+import { useRouter } from 'next/router';
 
-const App: NextPage = () => {
-  return (
-    <RelayEnvironmentProvider environment={environment}>
-      <Kiosk />
-    </RelayEnvironmentProvider>
-  );
+const App: NextPage = (props: any) => {
+  const session = loadSession();
+  const router = useRouter();
+  const roles = session?.user.roles;
+  if (roles && !roles.includes('admin')) {
+    router.push(roles[0]);
+    return null;
+  }
+  if (!roles) {
+    return null;
+  }
+  return <Kiosk />;
 };
 
 export default App;
--- pages/kiosk.tsx
+++ pages/kiosk.tsx
@@ -3,8 +3,6 @@ import { RelayEnvironmentProvider } from 'relay-hooks';
 import environment from '../src/relay/environment';
 import KioskPage from '../src/modules/kiosk/';
 import React from 'react';
-import { Breadcrumb } from 'antd';
-import { Header } from 'antd/lib/layout/layout';
 
 const Kiosk: NextPage = () => {
   return (
