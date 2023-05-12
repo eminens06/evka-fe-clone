98540008256132c9595f04b91792ac1677288aec
İsmet Emin Soyvural <79014987+eminens06@users.noreply.github.com>
2023-05-12 18:16:01 +0300

    Update PackagingDetail.tsx

--- src/modules/packaging/PackagingDetail.tsx
+++ src/modules/packaging/PackagingDetail.tsx
@@ -1,24 +1,73 @@
 import { Card, Col, Row, Typography } from 'antd';
-import React, { FC, useMemo } from 'react';
+import { message } from 'antd';
+import { InputNumber, Button } from 'antd';
+import React, { FC, useMemo, useState } from 'react';
+import UPDATE_PACKAGING_HAKEDIS, {
+  PackagingRelayUpdatePackagingHakedisMutation,
+} from '../../__generated__/PackagingRelayUpdatePackagingHakedisMutation.graphql';
+import { useMutation } from 'relay-hooks';
+import { WorkshopStatus } from '../production/types';
 
 interface Props {
+  productId: any;
+  status: any;
   productName: string;
+  marketplaceOrderId: string;
   isMonte: boolean;
   packageCount: number;
 }
+
 
 enum Status {
   DONE = 'Monte',
   NOT_DONE = 'Demonte',
 }
 
-const PackagingDetail: FC<Props> = ({ productName, isMonte, packageCount }) => {
+const PackagingDetail: FC<Props> = ({ productName, marketplaceOrderId, isMonte, packageCount, status }) => {
+
+  const [isActualPackagingNumberDifferent, setIsActualPackagingNumberDifferent] = useState(false);
+  const [actualNumber, setActualNumber] = useState<any>();
+  const progressStatus = status as WorkshopStatus;
+
+  const [
+    updateHakedis,
+  ] = useMutation<PackagingRelayUpdatePackagingHakedisMutation>(
+    UPDATE_PACKAGING_HAKEDIS, {
+    onError: (error: any) => {
+      message.error('Hata! ', error.response.errors[0].message);
+    },
+    onCompleted: (res) => {
+      message.success('Kullanılan Paket Adeti Başarıyla Güncellendi');
+      setIsActualPackagingNumberDifferent(!isActualPackagingNumberDifferent);
+    },
+  });
+
+  const onChangeOfNumber = (value: number) => {
+    setActualNumber(value);
+  };
+
+  const onDifferentButtonClick = () => {
+    setIsActualPackagingNumberDifferent(!isActualPackagingNumberDifferent);
+  };
+
+  const onUpdateButtonClick = () => {
+    const input = {
+      productOrderId: String(marketplaceOrderId),
+      changeOfPackaging: actualNumber - packageCount,
+    };
+    updateHakedis({
+      variables: {
+        input,
+      },
+    });
+  };
+
   const isMonteText = useMemo(() => {
     return isMonte ? Status.DONE : Status.NOT_DONE;
   }, [isMonte]);
   return (
     <Row gutter={24}>
-      <Col span={24}>
+      <Col span={(status === 'I') ? 12 : 24}>
         <Card title="Ürün Bilgileri" bordered className="form-card">
           <Typography.Title level={5}>{productName}</Typography.Title>
           <br></br>
@@ -28,6 +77,27 @@ const PackagingDetail: FC<Props> = ({ productName, isMonte, packageCount }) => {
           <br></br>
         </Card>
       </Col>
+      {(status === 'I') &&
+        <Col span={12}>
+          <Card title="Gerçekleşen Paketleme Bilgileri" bordered className="form-card">
+            <Row style={{ display: "flex", justifyContent: "center", paddingTop: "20px", paddingBottom: "15px" }}>
+              <Button
+                size='large'
+                onClick={onDifferentButtonClick}
+                type="primary">
+                Kullanılan Paket Sayısı Farklı
+              </Button>
+            </Row>
+            <Col span={24}>
+              <Row gutter={24} style={{ display: "flex", justifyContent: "center", paddingTop: "10px", paddingBottom: "10px" }}>
+                <InputNumber min={1} size='large' disabled={!isActualPackagingNumberDifferent} defaultValue={packageCount} onChange={onChangeOfNumber} style={{ marginRight: "10px" }} />
+                <Button size='large' disabled={!isActualPackagingNumberDifferent} onClick={onUpdateButtonClick} type="primary">
+                  Paket Sayısını Güncelle
+                </Button>
+              </Row>
+            </Col>
+          </Card>
+        </Col>}
     </Row>
   );
 };
